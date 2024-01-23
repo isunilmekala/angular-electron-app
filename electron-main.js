f1b4ebdeaf037d810b8d27004981145169e7e812
Sunil Mekala <c-smekala@pa.gov>
2024-01-23 11:06:54 -0500

    ADDED: Electron Forge for electron packaging and distribution

---
+++ electron-main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require("electron");
+// import path = require('node:path');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    // webPreferences: {
+    //   preload: path.join(__dirname, 'preload.js')
+    // }
+  });
+
+  win.loadFile("dist/angular-electron-app/index.html");
+
+  win.openDevTools();
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
